b745cea3f2e80eb7997435e9822228f2a4c73933
shinn lancelot <945226793@qq.com>
2018-12-09 23:57:24 +0800

    1. Common.php新增getNonce函数

--- src/Common.php
+++ src/Common.php
@@ -31,5 +31,33 @@ class Common
         curl_close($curl);
         return $output;
     }
+
+    /**
+     * 获取字符串
+     * @param int $type
+     * @param int $length
+     * @return string
+     */
+    public static function getNonce($type = 1, $length = 16)
+    {
+        $nonce = '';
+        switch ($type) {
+            case 1:
+                $string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+                break;
+            case 2:
+                $string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                break;
+            case 3:
+                $string = '0123456789';
+                break;
+            default:
+                $string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        }
+        for ($i = 0; $i < $length; $i++) {
+            $nonce .= substr($string, mt_rand(0, strlen($string) - 1), 1);
+        }
+        return $nonce;
+    }
 }
 
