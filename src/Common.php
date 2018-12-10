e3e96f4118bb07141bddbbda30b3c70f5f076040
shinn lancelot <945226793@qq.com>
2018-12-10 09:31:33 +0800

    1. 修改Common中httpRequest函数

--- src/Common.php
+++ src/Common.php
@@ -18,11 +18,9 @@ class Common
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HEADER, false);
         curl_setopt($curl, CURLOPT_TIMEOUT, 30);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: Application/json'));
         if (!empty($data)) {
             curl_setopt($curl, CURLOPT_POST, true);
             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
