9a3e37e1f2168d835333d8d05b04ffdb52b670fe
shinn lancelot <945226793@qq.com>
2019-01-07 10:28:53 +0800

    1. Common.php新增isWin、getNonceByURandom、extendArrayData通用方法

--- src/Common.php
+++ src/Common.php
@@ -31,6 +31,19 @@ class Common
     }
 
     /**
+     * 是否为windows系统
+     * @return bool
+     */
+    public static function isWin()
+    {
+        if (strtoupper(substr(PHP_OS,0,3)) === 'WIN') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
      * 获取字符串
      * @param int $type
      * @param int $length
@@ -56,6 +69,50 @@ class Common
             $nonce .= substr($string, mt_rand(0, strlen($string) - 1), 1);
         }
         return $nonce;
+    }
+
+    /**
+     * 根据URandom获取字符串
+     * @param int $length
+     * @return bool|string
+     */
+    public static function getNonceByURandom($length = 16)
+    {
+        $nonce = '';
+        $binary = '';
+        $fp = @fopen('/dev/urandom', 'rb');
+        if ($fp !== false) {
+            $binary .= @fread($fp, $length);
+            @fclose($fp);
+        } else {
+            trigger_error('Can not open /dev/urandom.');
+        }
+        $string = base64_encode($binary);
+        $nonce = substr($string, 0, $length);
+        return $nonce;
+    }
+
+    /**
+     * 扩展原数组的数据（仅更新原数组已有的键）
+     * @param $rawData
+     * @param $newData
+     * @return array
+     */
+    public static function extendArrayData($rawData, $newData)
+    {
+        if (!is_array($rawData) || count($rawData) <= 0) {
+            return array();
+        }
+
+        if (!is_array($newData)) {
+            return $rawData;
+        }
+
+        foreach ($rawData as $key=>$value) {
+            isset($newData[$key]) && $rawData[$key] = $newData[$key];
+        }
+
+        return $rawData;
     }
 
     /**
