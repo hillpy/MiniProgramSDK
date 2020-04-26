f724a98b6a92f211b35cc41720a3cd1f59016ec5
shinn_lancelot <lanhaichun0317@gmail.com>
2020-04-26 13:18:23 +0800

    refactor: 调整param.php

---
+++ src/Param.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Hillpy\MiniProgramSDK;
+
+class Param
+{
+    public static $getAccessToken = [
+        'url_param' => [
+            'grant_type' => 'client_credential',
+            'appid' => '',
+            'secret' => ''
+        ],
+        'form_data' => []
+    ];
+}
