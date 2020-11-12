20b388be3024c621b232dd887bfd3f018e1fec17
shinn_lancelot <lanhaichun0317@gmail.com>
2020-11-12 15:17:21 +0800

    refactor: 修改UpdatableMessageTest测试文件

--- tests/UpdatableMessage/UpdatableMessageTest.php
+++ tests/UpdatableMessage/UpdatableMessageTest.php
@@ -35,12 +35,20 @@ class UpdatableMessageTest extends TestCase
             'template_info' => [
                 'parameter_list' => [
                     [
-                        'name' => 'member_count',
-                        'value' => 1,
+                        'name' => 'member_count',   // target_state=0时必传
+                        'value' => '1', // 注意，此处要传字符串
                     ],
                     [
-                        'name' => 'room_limit',
-                        'value' => 5,
+                        'name' => 'room_limit', // target_state=0时必传
+                        'value' => '5', // 注意，此处要传字符串
+                    ],
+                    [
+                        'name' => 'path',   // target_state=1时必传
+                        'value' => 'pages/index/index?foo=bar',
+                    ],
+                    [
+                        'name' => 'version_type',   // target_state=1时必传
+                        'value' => 'develop',
                     ],
                 ],
             ],
